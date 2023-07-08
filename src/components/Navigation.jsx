e97654b10c0eeaf2e65a058b6125521d49c8efa3
Marvin Berrio <marvinccp@gmail.com>
2023-07-08 02:18:15 +0200

    Resolve Navigation styles module

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import styles from "../styles/navigation.module.css";
+import styles from "../styles/Navigation.module.css";
 import Image from "next/image";
 
 export const Navigation = () => {
